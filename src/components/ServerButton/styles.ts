9ea20368b7b3041ef3932e318f17ece858cd93ef
Vitor Bertti <berttivitor@gmail.com>
2020-06-20 19:11:55 -0300

    Created server name component

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -10,8 +10,47 @@ export const Button = styled.button<Props>`
    width: 48px;
    height: 48px;
    margin-bottom: 8px;
-   border-radius: 35%;
+   border-radius: 50%;
    background-color: ${(props) => (props.isHome ? 'var(--home)' : 'var(--primary)')};
    position: relative;
    cursor: pointer;
+   transition: border-radius 0.2s, background-color 0.2s;
+
+   &:active,
+   &:hover {
+      border-radius: 16%;
+      background-color: ${(props) => (props.isHome ? 'var(--home)' : 'var(--discord)')};
+   }
+
+   &::before {
+      width: 9px;
+      height: 9px;
+      position: absolute;
+      left: -17px;
+      top: calc(50% - 4.5px);
+      background-color: var(--white);
+      border-radius: 50%;
+      content: '';
+      display: ${(props) => (props.hasNotifications ? 'inline' : 'none')};
+   }
+
+   &::after {
+      background-color: var(--notification);
+      width: auto;
+      height: 16px;
+      padding: 0 4px;
+      position: absolute;
+      bottom: -4px;
+      right: -4px;
+      border-radius: 12px;
+      border: 4px solid var(--quaternary);
+      text-align: right;
+      font-size: 13px;
+      font-weight: bold;
+      color: var(--white);
+      content: '${(props) => props.mentions && props.mentions}';
+      display: ${(props) => (props.mentions && props.mentions > 0 ? 'inline' : 'none')};
+   }
+
+   
 `;
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+const ServerName: React.FC = () => {
+   return (
+      <Container>
+         <Title> Server Test </Title>
+         <ExpandIcon />
+      </Container>
+   );
+};
+
+export default ServerName;
