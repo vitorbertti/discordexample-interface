6a284cc024e869c3b4c127a4096c928fb138619d
Vitor Bertti <berttivitor@gmail.com>
2020-06-24 12:48:33 -0300

    Created channel message component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
 const ChannelData: React.FC = () => {
+   const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+   useEffect(() => {
+      const div = messagesRef.current;
+      if (div) {
+         div.scrollTop = div.scrollHeight;
+      }
+   }, [messagesRef]);
+
    return (
       <Container>
-         <Messages />
+         <Messages ref={messagesRef}>
+            <ChannelMessage author="Nick" date="24/06/2020" content="Hello World" />
+            <ChannelMessage
+               author="Nick2"
+               date="24/06/2020"
+               hasMention
+               isBot
+               content={
+                  <>
+                     <Mention>@Nick</Mention>, Hello
+                  </>
+               }
+            />
+         </Messages>
          <InputWrapper>
             <Input type="text" placeholder="Message #general" />
             <InputIcon />
